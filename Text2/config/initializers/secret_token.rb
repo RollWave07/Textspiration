87c808090eff62bdc7055c28a085d59634e0663a
MakerSquare Student <students@themakersquare.com>
2013-12-14 14:40:14 -0600

    init commit wth gems ready for heroku

---
+++ Text2/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Text2::Application.config.secret_key_base = 'cf730fca374c2649fbbfcd349d8a5453d11b1d12d58088b0b22ea7cb7f63567f05215bd2d1ee1c50833d8efb6ddd0b8d5038be80fcf657ecd4c5b25f35ecb90d'
---
+++ Text2/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Text2::Application.config.session_store :cookie_store, key: '_Text2_session'
