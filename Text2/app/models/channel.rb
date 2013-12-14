c6db3d337ff9f5be3d8624997da2bddf28e6ff5c
MakerSquare Student <students@themakersquare.com>
2013-12-14 14:50:59 -0600

    added the relationships in the model files

--- Text2/app/models/channel.rb
+++ Text2/app/models/channel.rb
@@ -1,2 +1,4 @@
 class Channel < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :type
 end
--- Text2/app/models/type.rb
+++ Text2/app/models/type.rb
@@ -1,2 +1,5 @@
 class Type < ActiveRecord::Base
+  has_many :channels
+  has_many :users, through: :channels
+  has_many :messages
 end
